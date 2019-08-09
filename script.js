5f1ec78143e001976c02edadcf7a40038e44ec23
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-09 13:10:47 -0700

    added functino to show translation

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+
+//Translate function
 async function translate (text,lang){
   const key = 'trnsl.1.1.20190809T190304Z.e2847c7e39a1bd65.4540cb358a217b65136cfb4287772ae03d1e2cb6';
   const [t, l] = [text,lang];
   try{
     const result = await fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${t}&lang=${l}`);
     const data = await result.json();
-    console.log(data);
+    return data;
   }catch(error){
     console.log(error);
   }
+}
+
+//Show Translation
+const showTranslation = () => {
+  const tag = document.querySelector('#demo1');
+  translate(tag.innerHTML,'zh').then(data => {
+    return data.text[0];
+  })
 }
 
 
