89a4b252d221b6ec4f8856a26ee4a33a849d9f6c
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-10 13:07:19 -0700

    added grid to the display

--- script.js
+++ script.js
@@ -9,8 +9,29 @@ var loopAlternate = anime({
 
 //sepcific unit
 var specificUnitValue = anime({
-  targets: '#specificUnitValue .pokemon',
+  targets: '#specificUnitValue .pokemon2',
   translateX: '1vh',
   rotate: '0.5turn',
-  duration: 50000
+  duration: 40000
 });
+
+//function based value
+var functionBasedPropVal = anime({
+  targets: '#functionBasedPropVal .el',
+  translateX: function(el) {
+    return el.getAttribute('data-x');
+  },
+  translateY: function(el, i) {
+    return 50 + (-50 * i);
+  },
+  scale: function(el, i, l) {
+    return (l - i) + .25;
+  },
+  rotate: function() { return anime.random(-360, 360); },
+  duration: function() { return anime.random(1200, 1800); },
+  duration: function() { return anime.random(800, 1600); },
+  delay: function() { return anime.random(0, 1000); },
+  direction: 'alternate',
+  loop: true
+});
+
