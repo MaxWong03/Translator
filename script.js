bc0c3523de05e0e6301f6d0a7c76f16061262d61
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-10 12:48:50 -0700

    experimented with specific unit

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-
-
-//Translate function
-async function translate (text,lang){
-  const key = 'trnsl.1.1.20190809T190304Z.e2847c7e39a1bd65.4540cb358a217b65136cfb4287772ae03d1e2cb6';
-  const [t, l] = [text,lang];
-  try{
-    const result = await fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${t}&lang=${l}`);
-    const data = await result.json();
-    return data;
-  }catch(error){
-    console.log(error);
-  }
-}
-
-//Show Translation
-const showTranslation = () => {
-  const tag = document.querySelector('#demo1');
-  translate(tag.innerHTML,'zh').then(data => {
-    tag.innerHTML = data.text[0];
-  })
-}
+// loop 10 times
 
 var loopAlternate = anime({
   targets: '#loopAlternate .pokemon',
   translateX: 250,
   loop: 10,
   direction: 'alternate'
+});
+
+//sepcific unit
+var specificUnitValue = anime({
+  targets: '#specificUnitValue .pokemon',
+  translateX: '1vh',
+  rotate: '0.5turn',
+  duration: 50000
 });
